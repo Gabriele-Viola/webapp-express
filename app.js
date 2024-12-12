f4151748cd327dc9f4618871c691a8a82b451317
Gabriele-Viola <gabrieleviola50@gmail.com>
2024-12-12 14:32:22 +0100

    add average query

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const server = express()
+const cors = require('cors')
 const FilmRouter = require('./routes/films.js')
 const NotFound = require('./middleware/NotFound.js')
 const ServerErrorHandler = require('./middleware/ServerErrorHandler.js')
 
 const HOST = process.env.HOST
 const PORT = process.env.PORT
+
+server.use(cors())
 
 server.get('/', (req, res) => {
     res.send('Server is up n Running')
--- controllers/FilmsController.js
+++ controllers/FilmsController.js
@@ -18,17 +18,25 @@ function show(req, res) {
 
     const reviewsSql = 'SELECT * FROM reviews WHERE movie_id=?'
 
+    const averageSql = 'SELECT movie_id, AVG(vote) AS average_vote FROM reviews WHERE movie_id=? GROUP BY movie_id'
+
     connection.query(sql, [id], (err, results) => {
         if (err) return res.status(500).json({ err: err })
         if (results.length == 0) return res.status(404).json({ err: 'book not found' })
 
         connection.query(reviewsSql, [id], (err, reviewsResults) => {
             if (err) return res.status(500).json({ err: err })
-            const film = {
-                ...results[0],
-                reviews: reviewsResults
-            }
-            res.json(film)
+
+            connection.query(averageSql, [id], (err, avgResults) => {
+                if (err) return res.status(500).json({ err: err })
+                const average = avgResults[0].average_vote
+                const film = {
+                    ...results[0],
+                    reviews: reviewsResults,
+                    average_vote: average
+                }
+                res.json(film)
+            })
         })
     })
 }
